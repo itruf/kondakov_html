f3e15c3ef7bc37830a051caa8352a7b9aa436ca6
Ivan <ivan.trufanov@me.com>
2016-04-02 12:39:50 +0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,43 @@
-console.log('This would be the main JS file.');
+//JS samples
+function winop() {
+    windop = window.open("http://www.google.ru", "GOOGLE", "height=120,width=300");
+}
+
+function wincl() {
+    windop.close();
+}
+
+//BOM samples
+function showAgent() {
+    alert(navigator.userAgent);
+}
+
+function showHref() {
+    alert(location.href);
+}
+
+//DOM samples
+function makeBGRed() {
+    if (document.body.style.background.valueOf() === new String("red").valueOf()) {
+        document.body.style.background = "";
+    } else {
+        document.body.style.background = "red";
+    }
+}
+
+function showSectionsCount() {
+    alert(document.getElementsByTagName('section').length);
+}
+
+//jQuery
+
+$(document).ready(function() {
+
+    $("#hideable_btn").click(function() {
+        $(this).hide();
+    });
+
+    $("#shake_btn").click(function() {
+        $(this).effect("shake");
+    });
+});
